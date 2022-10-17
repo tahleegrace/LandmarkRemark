91eefa9e15210491c020f83a74cc868b8d8fbb68
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-10-17 17:44:39 +1000

    Added landmark controller.

---
+++ LandmarkRemark.API/Controllers/LandmarkController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LandmarkRemark.API.Controllers
+{
+    /// <summary>
+    /// Provides functionality for managing landmarks.
+    /// </summary>
+    [Route("api/landmarks")]
+    [ApiController]
+    public class LandmarkController : ControllerBase
+    {
+        [HttpGet("test")]
+        public async Task<ActionResult<string>> Test()
+        {
+            // TODO: Remove this method once a working landmark endpoint is implemented.
+            return "This is a test";
+        }
+    }
+}
