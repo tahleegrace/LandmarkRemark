14c2b4ba0ca908300bb90838923471800d321f85
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-10-18 20:25:54 +1000

    LND-2: Added DTOs for landmarks.

---
+++ LandmarkRemark.Models/Landmarks/CreateLandmarkRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace LandmarkRemark.Models.Landmarks
+{
+    /// <summary>
+    /// A request for creating a new landmark.
+    /// </summary>
+    public class CreateLandmarkRequest
+    {
+        /// <summary>
+        /// Gets or sets the longitude.
+        /// </summary>
+        public decimal Longitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the latitude.
+        /// </summary>
+        public decimal Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the notes about the landmark.
+        /// </summary>
+        public string Notes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ID of the user who created the landmark.
+        /// </summary>
+        // TODO: This should be worked out based on who is currently logged in.
+        public int UserId { get; set; }
+    }
+}
---
+++ LandmarkRemark.Models/Landmarks/LandmarkDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace LandmarkRemark.Models.Landmarks
+{
+    /// <summary>
+    /// DTO object for returning landmarks from the API.
+    /// </summary>
+    public class LandmarkDTO
+    {
+        /// <summary>
+        /// Gets or sets the ID of the landmark.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the longitude.
+        /// </summary>
+        public decimal Longitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the latitude.
+        /// </summary>
+        public decimal Latitude { get; set; }
+
+        /// <summary>
+        /// Gets or sets the notes about the landmark.
+        /// </summary>
+        public string Notes { get; set; }
+    }
+}
