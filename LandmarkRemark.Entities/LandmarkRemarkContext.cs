9b0fb8cc025f7db44b01ed4d01b89c0b684c13ce
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-10-18 10:49:27 +1000

    Added LandmarkRemark DB context and a migration to create the initial LandmarkRemark DB.

---
+++ LandmarkRemark.Entities/LandmarkRemarkContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LandmarkRemark.Entities
+{
+    /// <summary>
+    /// The Landmark Remark database context.
+    /// </summary>
+    public class LandmarkRemarkContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Creates a new instance of LandmarkRemarkContext.
+        /// </summary>
+        /// <param name="configuration">The configuration.</param>
+        public LandmarkRemarkContext(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(this._configuration.GetConnectionString("LandmarkRemarkContext"));
+        }
+    }
+}
---
+++ LandmarkRemark.Entities/Migrations/20221018004332_AddLandmarkRemarkDB.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LandmarkRemark.Entities.Migrations
+{
+    public partial class AddLandmarkRemarkDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
