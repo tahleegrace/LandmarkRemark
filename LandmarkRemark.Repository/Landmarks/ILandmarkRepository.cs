2b94395a4d04c3417167f618f59860d01034c008
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-10-18 17:01:24 +1000

    LND-2: Added landmark repository.

---
+++ LandmarkRemark.Repository/Landmarks/ILandmarkRepository.cs
@@ -0,0 +1,17 @@
+﻿using LandmarkRemark.Entities;
+
+namespace LandmarkRemark.Repository.Landmarks
+{
+    /// <summary>
+    /// A landmarks repository.
+    /// </summary>
+    public interface ILandmarkRepository
+    {
+        /// <summary>
+        /// Creates a new landmark.
+        /// </summary>
+        /// <param name="landmark">The landmark.</param>
+        /// <returns>The new landmark.</returns>
+        Task<Landmark> Create(Landmark landmark);
+    }
+}
---
+++ LandmarkRemark.Repository/Landmarks/LandmarkRepository.cs
@@ -0,0 +1,38 @@
+﻿using LandmarkRemark.Entities;
+
+namespace LandmarkRemark.Repository.Landmarks
+{
+    /// <summary>
+    /// The landmarks repository.
+    /// </summary>
+    public class LandmarkRepository : ILandmarkRepository
+    {
+        private readonly LandmarkRemarkContext _context;
+
+        /// <summary>
+        /// Creates a new instance of LandmarkRepository.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        public LandmarkRepository(LandmarkRemarkContext context)
+        {
+            this._context = context;
+        }
+
+        /// <summary>
+        /// Creates a new landmark.
+        /// </summary>
+        /// <param name="landmark">The landmark.</param>
+        /// <returns>The new landmark.</returns>
+        public async Task<Landmark> Create(Landmark landmark)
+        {
+            landmark.Created = DateTime.UtcNow;
+            landmark.Updated = DateTime.UtcNow;
+
+            this._context.Landmarks.Add(landmark);
+
+            await this._context.SaveChangesAsync();
+
+            return landmark;
+        }
+    }
+}
