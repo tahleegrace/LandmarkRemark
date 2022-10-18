0fc38ef09609d44ade83bfd24809abf59e598495
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-10-18 10:22:36 +1000

    Added BaseEntity class.

---
+++ LandmarkRemark.Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿namespace LandmarkRemark.Entities
+{
+    /// <summary>
+    /// An entity in Landmark Remark.
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// The date the entity was created.
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        /// The date the entity was last modified.
+        /// </summary>
+        public DateTime Updated { get; set; }
+
+        /// <summary>
+        /// Gets or sets whether the entity has been deleted.
+        /// </summary>
+        public bool Deleted { get; set; } = false;
+    }
+}
