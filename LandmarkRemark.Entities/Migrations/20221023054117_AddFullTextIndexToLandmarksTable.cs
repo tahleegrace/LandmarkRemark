173fa524d1b84cd3a5f3e195246082115bae59e8
Tahlee-Joy Grace <tahlee.grace@gmail.com>
2022-10-23 16:37:22 +1000

    LND-6: Added full text index to landmarks table.

---
+++ LandmarkRemark.Entities/Migrations/20221023054117_AddFullTextIndexToLandmarksTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LandmarkRemark.Entities.Migrations
+{
+    public partial class AddFullTextIndexToLandmarksTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // EF Core Code First doesn't support creating full text indexes natively.
+            migrationBuilder.Sql
+            (
+                sql: "CREATE FULLTEXT CATALOG ftCatalog AS DEFAULT;",
+                suppressTransaction: true
+            );
+
+            migrationBuilder.Sql
+            (
+                sql: "CREATE FULLTEXT INDEX ON Landmarks(Notes) KEY INDEX PK_Landmarks;",
+                suppressTransaction: true
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
